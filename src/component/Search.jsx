608df955d45f88436aa7b445c9b5509b1821c6d1
zinee <zinee1@naver.com>
2024-09-25 17:31:19 +0900

    검색추가

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,13 +1,13 @@
 import { useRef } from "react";
 import "../App.css";
 
-export default function Search(getUser) {
+export default function Search({ getUser }) {
   const searchRef = useRef();
 
   return (
     <div className="search">
       <input type="text" ref={searchRef} />
-      <button>검색</button>
+      <button onClick={() => getUser(searchRef.current.value)}>검색</button>
     </div>
   );
 }
